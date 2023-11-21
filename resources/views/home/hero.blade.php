e9c457472f600069bd2d4fda4b6c6577960c5ee9
clsdjo30 <lesergent.cedric@gmail.com>
2023-11-21 06:04:12 +0100

    Template : hero responsive

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -1,30 +1,29 @@
-
-<div class="w-full h-full pb-10 bg-gradient-to-r from-violet-700 from-10% via-violet-800
-    to-violet-900 to-70% ">
-    <div class="relative px-4 xl:px-0 container mx-auto md:flex items-center gap-8 ">
-        <div class="text-color w-full md:w-2/3 pt-16 lg:pt-32 xl:pt-12 lg:my-32">
-            <h1 class="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full xl:text-6xl
-            text-violet-200 font-sans font-bold">Voyance et tarot divinatoire
+<div class="w-full py-20 bg-gradient-to-r from-violet-700 from-10% via-violet-800
+    to-violet-900 to-70% lg:px-28">
+    <div class="w-full flex flex-col items-center md:flex-row px-4 lg:mx-20">
+        <div class="lg:order-2 ">
+            <img class="object-coverh-96"
+                 src="{{asset('images/header.png')}}"
+                 alt="sample page" role="img"
+            />
+        </div>
+        <div class="w-full px-4 lg:order-1 lg:w-1/2 lg:pl-28 flex flex-col justify-center lg:justify-end items-center gap-y-4 lg:gap-10">
+            <h1 class="mt-4 w-full text-center lg:text-left text-4xl lg:text-6xl text-violet-200 font-sans font-bold">
+                Voyance et tarot divinatoire
             </h1>
-            <div class="f-f-r text-base lg:text-lg pb-20 sm:pb-0 pt-10 xl:pt-6 text-violet-200">
-                <h2>Votre portail vers l'univers mystique du tarot et votre compagnon divinatoire
-                    personnalisé. Plongez dans votre avenir et découvrez les mystères qui l'entourent avec notre application de voyance et tarot divinatoire. Vous obtiendrez des réponses qui vous aideront à mieux comprendre et à prendre des décisions éclairées dans tous les domaines de votre vie.</h2>
-            </div>
-            <div class="lg:flex w-2/5 pt-10">
-                <a href='https://play.google.com/store/apps/details?id=com.digitalFy.IrisClaireApp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
 
+            <h2 class="w-full text-center lg:text-left text-lg lg:text-xl text-violet-200">
+                Notre tarot divinatoire devient <span class="font-bold">mobile </span>: obtenez des réponses instantanées à vos questions de vie avec
+                notre
+                application de voyance.
+            </h2>
+
+            <div class="w-full flex justify-center lg:justify-start items-center">
+                <a href='https://play.google.com/store/apps/details?id=com.digitalFy.IrisClaireApp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                    <img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'
+                         width="250"/>
+                </a>
             </div>
         </div>
-        <img class="w-full mt-8 md:mt-0 object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0"
-             src="{{asset('images/header.png')}}"
-             alt="sample page" role="img" />
-        <a class="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8
-         f-f-r py-4  bg-indigo-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg"
-            href='https://play.google.com/store/apps/details?id=com.digitalFy.IrisClaireApp'>
-                <img alt='Disponible sur Google Play' src='{{asset
-            ('icons/google-play-badge.png')}}'/>
-        </a>
-
-
     </div>
 </div>
