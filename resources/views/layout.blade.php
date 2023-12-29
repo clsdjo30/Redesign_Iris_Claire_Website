c1092033d510313b486a570de0c2d664c5cccfa8
clsdjo30 <lesergent.cedric@gmail.com>
2023-12-29 07:10:01 +0100

    change loading Font method

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,12 +11,11 @@
 
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&display=swap"
-        rel="stylesheet">
-
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&display=swap" media="print" onload="this.media='all'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&display=swap">
+    </noscript>
 
     <!-- Styles -->
     <!-- Scripts -->
