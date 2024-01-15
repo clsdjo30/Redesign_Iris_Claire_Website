64a2303d15021e42e50d61ff2c6260edfc85181c
clsdjo30 <lesergent.cedric@gmail.com>
2024-01-15 04:08:17 +0100

    Template: add google badge on baniiere pub in blog

--- resources/views/components/blog-aside.blade.php
+++ resources/views/components/blog-aside.blade.php
@@ -24,28 +24,30 @@
         <!-- banniere pub iris -->
         <div class="w-full px-2 md:px-3">
             <div
-                class="bg-gradient-to-r from-violet-700 from-10% via-violet-800 to-violet-900 to-70% py-3 px-2 rounded-md">
-                <h3 class="mt-10 text-xl font-bold text-violet-100 text-center">
+                class="flex-col justify-center items-center bg-gradient-to-r from-violet-700 from-10% via-violet-800 to-violet-900 to-70% py-3 px-2
+                 rounded-md">
+                <h3 class="mt-10 text-2xl font-bold text-violet-100 text-center">
                     Votre compagnon divinatoire personnalisé
                 </h3>
                 <div class="w-full h-full mt-10">
                     <img src="{{asset('images/header.webp')}}" alt="Application Iris claire sur mobile" class="object-cover h-64"/>
                 </div>
-                <div class="mb-6">
-                    <p class="text-sm mt-4 text-violet-200 max-w-xl text-justify">
+                <div class="mb-6  flex flex-col justify-center items-center px-5">
+                    <p class="text-md mt-4 text-violet-200 max-w-xl text-justify">
                         Plongez dans votre avenir et découvrez les mystères qui
                         l'entourent ! Vous obtiendrez des réponses qui vont aideront à
                         mieux vous comprendre et à prendre des décisions.
                     </p>
-                    <div class="w-full mt-20 flex flex-col gap-5">
-                        <button
-                            class="flex mx-auto text-violet-900 font-semibold rounded-full bg-violet-100 border-0 py-2 px-16 focus:outline-none hover:bg-violet-600 hover:text-violet-100 text-lg">
-                            App Store
-                        </button>
-                        <button
-                            class="flex mx-auto text-violet-900 font-semibold rounded-full bg-violet-100 border-0 py-2 px-16 focus:outline-none hover:bg-violet-600 hover:text-violet-100 text-lg">
-                            Play Store
-                        </button>
+                    <div class="w-full mt-10 flex justify-center">
+                        <a href='https://play.google.com/store/apps/details?id=com.digitalFy.IrisClaireApp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'
+                            class="flex justify-center"
+                            target="_blank"
+                        >
+                            <img alt='Disponible sur Google Play'
+                                 src='https://play.google.com/intl/en_us/badges/static/images/badges/fr_badge_web_generic.png'
+                                 class="w-2/4"
+                            />
+                        </a>
                     </div>
                 </div>
             </div>
--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -20,8 +20,7 @@
 
             <div class="w-full flex justify-center lg:justify-start items-center">
                 <a href='https://play.google.com/store/apps/details?id=com.digitalFy.IrisClaireApp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-                    <img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'
-                         width="250"/>
+                    <img alt='Disponible sur Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/fr_badge_web_generic.png'/>
                 </a>
             </div>
         </div>
